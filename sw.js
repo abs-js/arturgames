414c07bc4bfaf69aa31a5bb8f0c2fd328802c18f
abs-j <artur.2012.bernardo@gmail.com>
2026-09-19 14:48:57 -0300

    Add driftmania fallback files to service worker

--- sw.js
+++ sw.js
@@ -3,7 +3,8 @@ const SHELL = "ag-shell-v1";
 const MAX_CACHE_BYTES = 20 * 1024 * 1024;
 const FALLBACK_FILES = {
   denselands: ["icon.svg", "card.png", "manifest.json", "service-worker.js", "map.html", "wiki.html"],
-  hillfight: ["icon.svg", "card.png"]
+  hillfight: ["icon.svg", "card.png"],
+  driftmania: ["icon.svg", "card.png"]
 };
 
 self.addEventListener("install", (event) => {
